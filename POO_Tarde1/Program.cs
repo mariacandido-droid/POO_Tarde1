16b6c0e7edc515cd0d60435dd7eeb4ea8917434c
mariacandido-droid <maricandido75777@gmail.com>
2023-03-08 15:47:46 -0300

    Criacao das classes de cliente e da classe endereco

--- POO_Tarde1/Program.cs
+++ POO_Tarde1/Program.cs
@@ -1,13 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-using POO_Tarde1;
+﻿using POO_Tarde1;
 
 Produto p1 = new Produto(1,"Mouse xing ling", 10.9M);
 Produto p2 = new Produto(2, "Teclado multilaser", 20.9M);
 
 Console.WriteLine(p1.getDescricao() + " " + p1.getValor());
-Console.WriteLine(p2.getDescricao() + " " + p2.getValor());
 
 p1.setValor(29.9M);
-p1.setDescricao("Mouse Razor");
+p1.setDescricao("Mouse Razer");
 
 Console.WriteLine(p1.getDescricao() + " " + p1.getValor());
