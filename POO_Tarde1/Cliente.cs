be2f2ded5255feaf4f91c1d651c2cb362662d4aa
mariacandido-droid <maricandido75777@gmail.com>
2023-03-08 15:54:06 -0300

    Modificacao nas classes co cliente e do enderco

--- POO_Tarde1/Cliente.cs
+++ POO_Tarde1/Cliente.cs
@@ -6,7 +6,40 @@ using System.Threading.Tasks;
 
 namespace POO_Tarde1
 {
-    internal class Cliente
+    public class Cliente
     {
+        private int Id;
+        private string Nome;
+        private string Telefone;
+        public Cliente(int id,string nome,string telefone) 
+        {
+            Id = id;
+            Nome = nome;
+            Telefone = telefone;
+        }
+        public int getId()
+        {
+            return Id;
+        }
+        public void setId(int id)
+        {
+            Id = id;
+        }
+        public string getNome()
+        {
+            return Nome;
+        }
+        public void setNome(string nome)
+        {
+            Nome = nome;
+        }
+        public string getTelefone()
+        {
+            return Telefone;
+        }
+        public void setTelefone(string telefone)
+        {
+            Telefone = telefone;
+        }
     }
 }
--- POO_Tarde1/Endereco.cs
+++ POO_Tarde1/Endereco.cs
@@ -6,7 +6,71 @@ using System.Threading.Tasks;
 
 namespace POO_Tarde1
 {
-    internal class Endereco
+    public class Endereco
     {
+        int Id;
+        int Cep;
+        string Rua;
+        string Bairro;
+        string Cidade;
+        int Numero;
+        public Endereco(int id,int cep, string rua,string bairro,string cidade,int numero) 
+        {
+            Id = id;
+            Cep = cep;
+            Rua = rua;
+            Bairro = bairro;
+            Cidade = cidade;
+            Numero = numero;
+        }
+        public int getId()
+        {
+            return Id;
+        }
+        public void setId(int id)
+        {
+            Id = id;
+        }
+        public int getCep()
+        {
+            return Cep;
+        }
+        public void setCep(int cep)
+        {
+            Cep = cep;
+        }
+        public string getRua()
+        {
+            return Rua;
+        }
+        public void setRua(string rua)
+        {
+            Rua = rua;
+        }
+        public string getBairro()
+        {
+            return Bairro;
+        }
+        public void setBairro(string bairro)
+        {
+            Bairro = bairro;
+        }
+        public string getCidade()
+        {
+            return Cidade;
+        }
+        public void setCidade(string cidade)
+        {
+            Cidade = cidade;
+        }
+        public int getNumero()
+        {
+            return Numero;
+        }
+        public void setNumero(int numero)
+        {
+            Numero = numero;
+        }
+
     }
 }
